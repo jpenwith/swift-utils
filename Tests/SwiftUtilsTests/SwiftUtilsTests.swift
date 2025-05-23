e9a3f368aaaad17bf00637f0d0f9ba38c4b12a0b
me <steven.kauthner@gmail.com>
2025-05-23 11:28:51 +0100

    test: organize Duration tests into a test suite struct

--- Tests/SwiftUtilsTests/SwiftUtilsTests.swift
+++ Tests/SwiftUtilsTests/SwiftUtilsTests.swift
@@ -1,22 +1,18 @@
 import SwiftUtils
 import Testing
 
-@Test func testExample() throws {
-    // XCTest Documentation
-    // https://developer.apple.com/documentation/xctest
 
-    // Defining Test Cases and Test Methods
-    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-}
+@Suite("Duration")
+struct Duration {
+    @Test func testMinutes() {
+        #expect(Duration.minutes(77) == .seconds(4620))
+    }
 
-@Test func testMinutes() {
-    #expect(Duration.minutes(77) == .seconds(4620))
-}
+    @Test func testHours() {
+        #expect(Duration.hours(2) == .minutes(120))
+    }
 
-@Test func testHours() {
-    #expect(Duration.hours(2) == .minutes(120))
-}
-
-@Test func testDays() {
-    #expect(Duration.days(3) == .hours(72))
+    @Test func testDays() {
+        #expect(Duration.days(3) == .hours(72))
+    }
 }
