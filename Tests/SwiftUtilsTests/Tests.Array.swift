571643cfdf7441ec0e11fd7d42c784323c9ab3d5
me <steven.kauthner@gmail.com>
2025-05-24 20:37:37 +0100

    refactor: rename ArrayTests to Array and remove DateFormatter tests file

--- Tests/SwiftUtilsTests/Tests.Array.swift
+++ Tests/SwiftUtilsTests/Tests.Array.swift
@@ -3,7 +3,7 @@ import  SwiftUtils
 
 extension Tests {
     @Suite("Array")
-    struct ArrayTests {
+    struct Array {
         @Test func testRemovingFirstK() {
             let array = [1, 2, 3, 4, 5]
             let result = array.removingFirst(2)
