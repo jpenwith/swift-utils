394bc2a8a4ce50140aa5ff1372cb03137178f5bd
me <steven.kauthner@gmail.com>
2025-05-28 20:39:38 +0100

    feat: add StringEncoded and OptionalStringEncoded property wrappers for string-based Codable conversion

---
+++ Sources/SwiftUtils/Codable/StringEncoded.swift
@@ -0,0 +1,60 @@
+//
+//  StringEncoded
+//  swift-utils
+//
+//  Created by me on 28/05/2025.
+//
+
+@propertyWrapper
+public struct StringEncoded<Value: LosslessStringConvertible & Codable> {
+    public var wrappedValue: Value
+
+    public init(wrappedValue: Value) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension StringEncoded: Codable {
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        guard let value = Value(string) else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid \(Value.self) string: \(string)")
+        }
+        self.wrappedValue = value
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue.description)
+    }
+}
+
+@propertyWrapper
+public struct OptionalStringEncoded<Value: LosslessStringConvertible & Codable>: Codable {
+    public var wrappedValue: Value?
+
+    public init(wrappedValue: Value?) {
+        self.wrappedValue = wrappedValue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if container.decodeNil() {
+            self.wrappedValue = nil
+            return
+        }
+
+        let string = try container.decode(String.self)
+        self.wrappedValue = Value(string)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        if let value = wrappedValue {
+            try container.encode(value.description)
+        } else {
+            try container.encodeNil()
+        }
+    }
+}
